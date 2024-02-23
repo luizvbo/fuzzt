106882531b3d539b9f358ff046c310cdde9109c2
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-02-23 12:49:55 +0100

    Fix bug

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -14,23 +14,10 @@ macro_rules! assert_delta {
     };
 }
 
-#[cfg(feature = "damerau_levenshtein")]
 pub mod damerau_levenshtein;
-
-#[cfg(feature = "gestalt")]
 pub mod gestalt;
-
-#[cfg(feature = "hamming")]
 pub mod hamming;
-
-#[cfg(feature = "jaro")]
 pub mod jaro;
-
-#[cfg(feature = "levenshtein")]
 pub mod levenshtein;
-
-#[cfg(feature = "optimal_string_alignment")]
 pub mod optimal_string_alignment;
-
-#[cfg(feature = "sorensen_dice")]
 pub mod sorensen_dice;
